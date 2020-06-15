915c26435b2c11fd09113c67f40461f5fbefcb9f
yu2307 <jin43077@gmail.com>
2020-06-15 14:42:09 +0900

    에외처리

---
+++ ExceptionTestApp/ExceptionTestApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 100, y = 5, value = 0;
+
+
+
+            try
+            {
+                value = x / y;
+                //Console.Write("{0}/{1} = {2}", x, y, value);
+                Console.WriteLine($"{x}/{y} = {value}");
+                throw new Exception("1.사용자 에러");
+            }
+
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("2. y값을 0보다 크게 입력하세요");
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine("3." + ex.Message);
+               
+             }
+            finally
+            {
+                Console.WriteLine("4. 프로그램이 종료했습니다.");
+            }
+        }
+    }
+}
