318535957ab2fede5a9ba6b254304906da2db383
yu2307 <jin43077@gmail.com>
2020-06-15 12:34:39 +0900

    제네릭 소스 수정

--- genericapp/genericapp/Program.cs
+++ genericapp/genericapp/Program.cs
@@ -1,15 +1,49 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace genericapp
+namespace GenericApp
 {
+    public class SimpleGeneric<T>
+    {
+        private T[] values;
+        private int index;
+
+        public SimpleGeneric(int len) // Construtor
+        {
+            values = new T[len];
+            index = 0;
+        }
+
+        public void Add(params T[] args)
+        {
+            foreach (T e in args)
+            {
+                values[index++] = e;
+            }
+        }
+        public void Print()
+        {
+            foreach (T e in values)
+            {
+                Console.Write(e + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            SimpleGeneric<Int32> gInteger = new SimpleGeneric<Int32>(10);
+            SimpleGeneric<Double> gDouble = new SimpleGeneric<Double>(10);
+
+            gInteger.Add(1, 2);
+            gInteger.Add(1, 2, 3, 4, 5, 6, 7);
+            gInteger.Add(10);
+            gInteger.Print();
+
+            gDouble.Add(10.0, 12.5, 37.4);
+            gDouble.Print();
         }
     }
 }
